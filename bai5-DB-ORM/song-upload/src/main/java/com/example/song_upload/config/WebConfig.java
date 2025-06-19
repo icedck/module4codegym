847bf9bffb9e630977258032c6a43645f6943e0c
icedck <charm30072002@gmail.com>
2025-06-19 20:07:30 +0700

    update bai5

--- bai5-DB-ORM/song-upload/src/main/java/com/example/song_upload/config/WebConfig.java
+++ bai5-DB-ORM/song-upload/src/main/java/com/example/song_upload/config/WebConfig.java
@@ -1,8 +1,10 @@
 package com.example.song_upload.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
