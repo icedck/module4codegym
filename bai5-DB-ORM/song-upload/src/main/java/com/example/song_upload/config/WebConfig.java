407589019f5debbb9147b415b94a9a35f6110b51
icedck <charm30072002@gmail.com>
2025-06-19 15:26:09 +0700

    add bai5

---
+++ bai5-DB-ORM/song-upload/src/main/java/com/example/song_upload/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.example.song_upload.config;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/");
+    }
+}
